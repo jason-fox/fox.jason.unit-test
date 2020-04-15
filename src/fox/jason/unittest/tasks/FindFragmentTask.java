f12187e6a08ea9b6b77fe9c9738bad77f28d520c
Jason Fox <jason.fox@fiware.org>
2020-04-15 23:46:53 +0200

    Remove dependence on scriptdef tasks.

---
+++ src/fox/jason/unittest/tasks/FindFragmentTask.java
@@ -0,0 +1,34 @@
+/*
+ *  This file is part of the DITA-OT Unit Test Plug-in project.
+ *  See the accompanying LICENSE file for applicable licenses.
+ */
+
+package fox.jason.unittest.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+// Task to extract the string between <article> tags.
+
+public class FindFragmentTask extends Task {
+
+  /**
+   * Creates a new <code>FindFragmentTask</code> instance.
+   */
+  public FindFragmentTask() {
+    super();
+  }
+
+  /**
+   * Method execute.
+   *
+   * @throws BuildException if something goes wrong
+   */
+  @Override
+  public void execute() {
+    String input = project.getProperty("htmlSource");
+    int start = input.indexOf("<article");
+    int end = input.indexOf("</article>") + 10;
+    getProject().setProperty("fragment", input.substring(start, end));
+  }
+}
