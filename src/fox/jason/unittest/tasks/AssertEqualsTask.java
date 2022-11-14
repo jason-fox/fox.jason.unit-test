1ffe15dc2b3514da9553606ee5fa74b8915c3d8d
GitHub Action <action@github.com>
2022-11-14 19:24:54 +0100

    Revert "Release preparation"
    This reverts commit 4e509b7431b08053d0731effba944061ef1f8abb.

---
+++ src/fox/jason/unittest/tasks/AssertEqualsTask.java
@@ -0,0 +1,116 @@
+/*
+ *  This file is part of the DITA-OT Unit Test Plug-in project.
+ *  See the accompanying LICENSE file for applicable licenses.
+ */
+
+package fox.jason.unittest.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Exit;
+
+//  Custom  task to invoke a conditional <fail>.
+
+public class AssertEqualsTask extends Task {
+
+  /**
+   * Field body.
+   */
+  private String body;
+  /**
+   * Field message.
+   */
+  private String message;
+  /**
+   * Field arg1.
+   */
+  private String arg1;
+  /**
+   * Field arg2.
+   */
+  private String arg2;
+
+  /**
+   * Creates a new <code>AssertEqualsTask</code> instance.
+   */
+  public AssertEqualsTask() {
+    super();
+    this.body = "";
+    this.message = "";
+    this.arg1 = null;
+    this.arg2 = null;
+  }
+
+  /**
+   * Method setBody.
+   *
+   * @param body String
+   */
+  public void setBody(String body) {
+    this.body = body;
+  }
+
+  /**
+   * Method setMessage.
+   *
+   * @param message String
+   */
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  /**
+   * Method setArg1.
+   *
+   * @param arg1 String
+   */
+  public void setArg1(String arg1) {
+    this.arg1 = arg1;
+  }
+
+  /**
+   * Method setArg2.
+   *
+   * @param arg2 String
+   */
+  public void setArg2(String arg2) {
+    this.arg2 = arg2;
+  }
+
+  /**
+   * Method execute.
+   *
+   * @throws BuildException if something goes wrong
+   */
+  @Override
+  public void execute() {
+    //  @param arg1 - first argument for comparison
+    //  @param arg2 - second argument for comparison
+    // @param body - detail of the error message
+    //  @param message - short text description of the error
+    if (arg1 == null) {
+      throw new BuildException("You must supply a first argument for comparison");
+    }
+    if (arg2 == null) {
+      throw new BuildException("You must supply a second argument for comparison");
+    }
+
+    boolean os = "true".equals(getProject().getProperty("test.os.matches"));
+
+    if (os && !arg1.equals(arg2)) {
+      String escapeCode = Character.toString((char) 27);
+      boolean colorize = "true".equals(getProject().getProperty("cli.color"));
+
+      if (colorize) {
+        message = escapeCode + "[31m" + message + escapeCode + "[0m";
+      }
+      getProject().log("", 1);
+      getProject().log(message, 1);
+
+      Exit task = (Exit) getProject().createTask("fail");
+      task.setMessage(body);
+
+      task.perform();
+    }
+  }
+}
---
+++ src/fox/jason/unittest/tasks/CopyResultTask.java
@@ -0,0 +1,84 @@
+/*
+ *  This file is part of the DITA-OT Unit Test Plug-in project.
+ *  See the accompanying LICENSE file for applicable licenses.
+ */
+
+package fox.jason.unittest.tasks;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.Copy;
+
+// if test.copy.dir is set, make a copy of the expectation file
+// This allows us to refresh the test expectations
+
+public class CopyResultTask extends Task {
+
+  /**
+   * Field src.
+   */
+  private String src;
+
+  /**
+   * Field dest.
+   */
+  private String dest;
+
+  /**
+   * Creates a new <code>CopyResultTask</code> instance.
+   */
+  public CopyResultTask() {
+    super();
+    this.src = null;
+    this.dest = null;
+  }
+
+  /**
+   * Method setSrc.
+   *
+   * @param src String
+   */
+  public void setSrc(String src) {
+    this.src = src;
+  }
+
+  /**
+   * Method setDest.
+   *
+   * @param dest String
+   */
+  public void setDest(String dest) {
+    this.dest = dest;
+  }
+
+  /**
+   * Method execute.
+   *
+   * @throws BuildException if something goes wrong
+   */
+  @Override
+  public void execute() {
+    // @param dest - destination of the expecation
+    // @param src -  source of the expecation
+    if (src == null) {
+      throw new BuildException("You must supply a source of the expecation");
+    }
+    if (dest == null) {
+      throw new BuildException("You must supply a destination of the expecation");
+    }
+
+    boolean os = "true".equals(getProject().getProperty("test.os.matches"));
+
+    String rootDir = getProject().getProperty("test.root.dir");
+    String testCopyDir = getProject().getProperty("test.copy.dir");
+    boolean testCopy = "true".equals(getProject().getProperty("test.copy"));
+
+    if (testCopy && os) {
+      Copy task = (Copy) getProject().createTask("copy");
+      task.setFile(new java.io.File(src));
+      task.setTofile(new java.io.File(dest.replace(rootDir, testCopyDir)));
+      task.setOverwrite(true);
+      task.perform();
+    }
+  }
+}
